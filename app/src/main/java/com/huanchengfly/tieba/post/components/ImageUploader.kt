db31f1858c4dda0924a8520df1e6ceb5040349ca
kmcbest <kmc.best@qq.com>
2026-08-13 15:20:57 +0800

    revert: 还原水印上传参数与 zzc10086 上游主线保持完全一致

--- app/src/main/java/com/huanchengfly/tieba/post/components/ImageUploader.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/components/ImageUploader.kt
@@ -166,6 +166,7 @@ class ImageUploader(
                 setType(MyMultipartBody.FORM)
                 addFormDataPart("alt", "json")
                 addFormDataPart("chunkNo", "${chunk + 1}")
+                if (forumName.isNotEmpty()) addFormDataPart("forum_name", forumName)
                 addFormDataPart("groupId", "1")
                 addFormDataPart("height", "$height")
                 addFormDataPart("isFinish", isFinish.booleanToString())
@@ -174,13 +175,9 @@ class ImageUploader(
                 addFormDataPart("resourceId", "$fileMd5$chunkSize")
                 addFormDataPart("saveOrigin", isOriginImage.booleanToString())
                 addFormDataPart("size", "$fileLength")
+                if (forumName.isNotEmpty()) addFormDataPart("small_flow_fname", forumName)
                 addFormDataPart("width", "$width")
                 addFormDataPart("chunk", "file", chunkBytes.toRequestBody())
-
-                if (picWatermarkType == PIC_WATER_TYPE_FORUM_NAME && forumName.isNotEmpty()) {
-                    addFormDataPart("forum_name", forumName)
-                    addFormDataPart("small_flow_fname", forumName)
-                }
             }
         }
 
